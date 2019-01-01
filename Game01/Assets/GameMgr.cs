6c55c11a1c0a5e4569f44f0b3dff0d0b2b5ea0a8
Mats Lundberg <mats.lundberg@gmail.com>
2019-01-01 21:12:11 +0200

    Almost a game... :-)

---
+++ Game01/Assets/GameMgr.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMgr : MonoBehaviour
+{
+    bool gameOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EndGame()
+    {
+        if(gameOver == false)
+        {
+            gameOver = true;
+            Debug.Log("END!");
+            Invoke("Restart", 2f);
+
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Game01/Assets/PlayerObstacle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObstacle : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collisionInfo) {
+        if (collisionInfo.collider.tag == "obstacle")
+        {
+            movement.enabled = false;
+
+            FindObjectOfType<GameMgr>().EndGame();
+        }
+    }
+}
